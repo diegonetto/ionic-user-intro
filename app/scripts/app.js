d962350fa2943e7bc149257dbbcaaae5356e0e8c
Diego Netto <diegormnetto@gmail.com>
2014-05-13 21:06:32 -0500

    Refactor to match CodePen Example

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -4,30 +4,20 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 angular.module('<%= appName %>', ['ionic', '<%= appName %>.controllers'])
 
-.run(function($ionicPlatform) {
-  $ionicPlatform.ready(function() {
-    if(window.StatusBar) {
-      StatusBar.styleDefault();
-    }
-  });
-})
-
 .config(function($stateProvider, $urlRouterProvider) {
+
   $stateProvider
+  .state('intro', {
+    url: '/',
+    templateUrl: 'templates/intro.html',
+    controller: 'IntroCtrl'
+  })
+  .state('main', {
+    url: '/main',
+    templateUrl: 'templates/main.html',
+    controller: 'MainCtrl'
+  });
 
-    .state('intro', {
-      url: '/',
-      templateUrl: 'templates/intro.html',
-      controller: 'IntroCtrl'
-    })
-
-    .state('app', {
-      url: '/app',
-      templateUrl: 'templates/main.html',
-      controller: 'AppCtrl'
-    });
-
-  // if none of the above states are matched, use this as the fallback
   $urlRouterProvider.otherwise('/');
-});
 
+});
