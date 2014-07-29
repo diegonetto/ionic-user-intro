117f0ef1930a3c88fe64247f2a57e95393079ade
Diego Netto <diegormnetto@gmail.com>
2014-07-29 17:34:01 -0500

    Include .run() with Ionic keyboard plugin configuration.

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -4,6 +4,20 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 angular.module('<%= appName %>', ['ionic', 'config', '<%= appName %>.controllers'])
 
+.run(function($ionicPlatform) {
+  $ionicPlatform.ready(function() {
+    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+    // for form inputs)
+    if(window.cordova && window.cordova.plugins.Keyboard) {
+      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+    }
+    if(window.StatusBar) {
+      // org.apache.cordova.statusbar required
+      StatusBar.styleDefault();
+    }
+  });
+})
+
 .config(function($stateProvider, $urlRouterProvider) {
 
   $stateProvider
